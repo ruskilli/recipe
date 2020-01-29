bc246838893f65e9665df1cf90609b9a21ebe322
Rune Nordbøe Skillingstad <runesk@gmail.no>
2020-01-29 14:21:34 +0100

    merge template into main json file

--- libs/parser.php
+++ libs/parser.php
@@ -9,23 +9,29 @@ function get_result($style, $format) {
     return;
   }
 
-  $template = json_decode(file_get_contents("styles/template.json"),true);
-  check_json();
-
   $result = "";
-
-  if($data && isset($data["count"]["recipes"])) {
-    for($r = 0; $r < $data["count"]["recipes"]; $r++) {
-      for($o = 0; $o < sizeof($template["order"]); $o++) {
-	$count = rand($data["count"][$template["order"][$o]][0],$data["count"][$template["order"][$o]][1]);
+  if($data && isset($data["template"]["count"]["recipes"])) {
+    // number of recipes to generate
+    for($r = 0; $r < $data["template"]["count"]["recipes"]; $r++) {
+      // Use template order 
+      for($o = 0; $o < sizeof($data["template"]["order"]); $o++) {
+	// Each part might have a random to:from in template
+	$count = rand($data["template"]["count"][$data["template"]["order"][$o]][0],
+	              $data["template"]["count"][$data["template"]["order"][$o]][1]);
         for($c = 0; $c < $count; $c++) {
-           $result .= $data["style"][$format][$template["order"][$o]]["pre"];
-           for($p = 0; $p < sizeof($template["parts"][$template["order"][$o]]); $p++) {
-              $rnd = array_rand($data[$template["order"][$o]][$template["parts"][$template["order"][$o]][$p]]);
-              $result .= $data[$template["order"][$o]][$template["parts"][$template["order"][$o]][$p]][$rnd];
-              $result .= $data["style"][$format][$template["order"][$o]]["each"];
+	  // Prepend style for given part
+           $result .= $data["template"]["style"][$format][$data["template"]["order"][$o]]["pre"];
+	   // Loop subparts
+	   for($p = 0; $p < sizeof($data["template"]["parts"][$data["template"]["order"][$o]]); $p++) {
+	      // Randomize item from array
+              $rnd = array_rand($data[$data["template"]["order"][$o]][$data["template"]["parts"][$data["template"]["order"][$o]][$p]]);
+	      // Append result
+ 	      $result .= $data[$data["template"]["order"][$o]][$data["template"]["parts"][$data["template"]["order"][$o]][$p]][$rnd];
+	      // Append style for each subpart
+              $result .= $data["template"]["style"][$format][$data["template"]["order"][$o]]["each"];
            }
-           $result .= $data["style"][$format][$template["order"][$o]]["post"] . "\n";
+	   // Append post style for given part
+           $result .= $data["template"]["style"][$format][$data["template"]["order"][$o]]["post"] . "\n";
         }
       }
     }
