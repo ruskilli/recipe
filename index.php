ad195a577b45e80d5dce3c119a6b2aa138d8a7ef
Rune Nordbøe Skillingstad <runesk@gmail.no>
2020-01-30 10:53:47 +0100

    nå med HTML-templat og CSS for food

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 @include_once "libs/include.php";
 
+@require "addons/dwoo-1.3.7/vendor/autoload.php";
+
 $style = get_style();
 
 if(isset($_GET['id']) && $_GET['id']) {
@@ -12,12 +14,18 @@ if(isset($_GET['id']) && $_GET['id']) {
 
 $result = isset($result) ? $result : get_result($style, "html");
 
-// TODO: Make some nice HTML 
-
-echo $result;
-
 if(!isset($id)) {
   $id = store($result);
 }
 
-echo "<a href='?id=$id'>Del</a> <a href='?id'>Ny</a>";
+$dwoo_core = new Dwoo\Core();
+$dwoo_core->setCompileDir($GLOBALS['dwoo_compile_dir']);
+
+$dwoo_tpl  = new Dwoo\Template\File("templates/html/food.tpl");
+$dwoo_data = new Dwoo\Data();
+
+$dwoo_data->assign('result', $result);
+$dwoo_data->assign('id'    , $id);
+
+echo $dwoo_core->get($dwoo_tpl, $dwoo_data);
+
---
+++ templates/html/food.tpl
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Matoppskrift</title>
+  <link rel="stylesheet" href="css/food.css" />
+</head>
+<body>
+<div class="result">
+{$result}
+</div>
+<div class="share">
+  <span class="share_id"><a href="?id={$id}">Del</a></span> - 
+  <span class="share_new"><a href="?id">Ny</a></span>
+</div>
+<div class="about">
+  <a href="https://github.com/ruskilli/recipe">Source at GitHub</a> - 
+  <img src="https://github.com/ruskilli/recipe/workflows/Build/badge.svg" alt="Build status" />
+</div>
+</body>
+</html>
